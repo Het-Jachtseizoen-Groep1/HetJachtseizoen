42d24921f7309a4380b8d46854bf916cf4badb21
BlommePieter <pieter.blomme@student.howest.be>
2023-01-16 11:20:24 +0100

    current locatie op marker in map
    Co-authored-by: DecorteKasper <DecorteKasper@users.noreply.github.com>

--- web app/script/script.js
+++ web app/script/script.js
@@ -1,19 +1,24 @@
 
+var map;
 
 function getlocation() {
     navigator.geolocation.getCurrentPosition(function (position) {
         console.log(position.coords.latitude);
         console.log(position.coords.longitude);
+
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+
+        var map = L.map('map').setView([lat, lon], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        var marker = L.marker([lat, lon]).addTo(map);
+        marker.bindPopup("Locatie Boeven").openPopup();
     });
 }
-
-
-
-
-
-
-
-
 
 
 document.addEventListener('DOMContentLoaded', function () {
