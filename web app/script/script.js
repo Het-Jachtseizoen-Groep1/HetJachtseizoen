8f2f04320db4111cab7e2f426ddb46f9d67c2603
BlommePieter <pieter.blomme@student.howest.be>
2023-01-16 14:06:42 +0100

    site Co-authored-by: DecorteKasper <DecorteKasper@users.noreply.github.com>

--- web app/script/script.js
+++ web app/script/script.js
@@ -9,18 +9,17 @@ function getlocation() {
         var lat = position.coords.latitude;
         var lon = position.coords.longitude;
 
-        var map = L.map('map').setView([lat, lon], 13);
+        var map = L.map('map').setView([lat, lon], 15);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
         var greenIcon = L.icon({
-            iconUrl: 'web app/assets/icons/map_marker.png',
+            iconUrl: '/web app/assets/map_marker.png',
 
-            iconSize: [38, 95], // size of the icon
-            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+            iconSize: [35, 47], // size of the icon
+            //iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
+            // popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
         });
 
         var marker = L.marker([lat, lon], { icon: greenIcon }).addTo(map);
