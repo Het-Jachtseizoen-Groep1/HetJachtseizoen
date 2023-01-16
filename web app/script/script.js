27a1dee3b097b54f8dda4aaa0b9ddde5225b7a27
BlommePieter <pieter.blomme@student.howest.be>
2023-01-16 12:07:19 +0100

    pages

--- web app/script/script.js
+++ web app/script/script.js
@@ -15,7 +15,15 @@ function getlocation() {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
-        var marker = L.marker([lat, lon]).addTo(map);
+        var greenIcon = L.icon({
+            iconUrl: 'web app/assets/icons/map_marker.png',
+
+            iconSize: [38, 95], // size of the icon
+            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+
+        var marker = L.marker([lat, lon], { icon: greenIcon }).addTo(map);
         marker.bindPopup("Locatie Boeven").openPopup();
     });
 }
